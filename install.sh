b3b200c6048763cbb2ad55b2044e70c6b988e731
Izzy Gibson <izzyyg27@gmail.com>
2022-08-20 13:40:29 -0700

    Modify i3 config

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Terminal colors
-BLACK = '\033[0;30m'
-DARK_GRAY = '\033[1;30m'
-RED = '\033[0;31m'
-LIGHT_RED = '\033[1;31m'
-GREEN = '\033[0;32m'
-LIGHT_GREEN = '\033[1;32m'
-ORANGE = '\033[0;33m'
-YELLOW = '\033[1;33m'
-BLUE = '\033[0;34m'
-LIGHT = '\033[1;34m'
-PURPLE = '\033[0;35m'
-LIGHT_PURPLE = '\033[1;35m'
-CYAN = '\033[0;36m'
-LIGHT_CYAN = '\033[1;36m'
-LIGHT_GRAY = '\033[0;37m'
-WHITE = '\033[1;37m'
-RESET = '\033[0m'
+BLACK='\033[0;30m'
+DARK_GRAY='\033[1;30m'
+RED='\033[0;31m'
+LIGHT_RED='\033[1;31m'
+GREEN='\033[0;32m'
+LIGHT_GREEN='\033[1;32m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+LIGHT='\033[1;34m'
+PURPLE='\033[0;35m'
+LIGHT_PURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LIGHT_CYAN='\033[1;36m'
+LIGHT_GRAY='\033[0;37m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
 
 # Clear the terminal and display a welcome message
 sudo ls
@@ -42,8 +42,8 @@ clear
 sudo wget https://isabellagibson.github.io/arch/assets/Raleway-Light.ttf -O /usr/share/fonts/Raleway-Light.ttf
 
 # Set wallpaper
-mkdir Pictures
-mkdir Pictures/Wallpapers
+mkdir ~/Pictures
+mkdir ~/Pictures/Wallpapers
 wget https://isabellagibson.github.io/arch/assets/wallpaper.png -O ~/Pictures/Wallpapers/arch.png
 feh --bg-fill ~/Pictures/Wallpapers/arch.png
 
@@ -62,4 +62,4 @@ sudo wget https://isabellagibson.github.io/arch/config/config.rasi -O ~/.config/
 sudo wget https://isabellagibson.github.io/arch/bashrc -O ~/.bashrc
 
 # Reboot!
-sudo reboot now
+# sudo reboot now
