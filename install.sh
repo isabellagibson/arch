56ecf5a94ed277cc553a8f38c5d64a467abeab3e
Izzy Gibson <izzyyg27@gmail.com>
2022-08-21 13:42:46 -0700

    Revert

--- install.sh
+++ install.sh
@@ -42,9 +42,8 @@ yes | sudo pacman -S neofetch alacritty net-tools nmap ufw zip unzip picom rofi 
 clear
 
 # Install fonts
-wget https://isabellagibson.github.io/arch/assets/fonts.zip -O ~/fonts.zip
-sudo unzip ~/fonts.zip /usr/share/fonts/
-rm ~/fonts.zip
+sudo wget https://isabellagibson.github.io/arch/assets/Raleway-SemiBold.ttf -O /usr/share/fonts/RalewaySemiBold.ttf
+sudo wget https://isabellagibson.github.io/arch/assets/Raleway-Regular.ttf -O /usr/share/fonts/RalewayRegular.ttf
 
 # Set wallpaper
 mkdir ~/Pictures
