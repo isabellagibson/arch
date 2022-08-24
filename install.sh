e53bb0baa867f53d0ff6bb40d0e87f00651b8db1
Izzy Gibson <izzyyg27@gmail.com>
2022-08-24 07:44:39 -0700

    Change install

--- install.sh
+++ install.sh
@@ -12,7 +12,8 @@ clear
 
 # Install alacritty & other packages
 echo "Installing packages..."
-yes | sudo pacman -S neofetch alacritty net-tools ufw zip unzip rofi feh polybar nemo coreutils inetutils discord nmap ttf-font-awesome ttf-fira-code playerctl rofi-emoji picom dunst libnotify yt-dlp ffmpeg python-pip gnome-keyring man-db
+yes | sudo pacman -S neofetch alacritty net-tools ufw zip unzip rofi feh polybar nemo coreutils inetutils discord nmap ttf-font-awesome ttf-fira-code playerctl rofi-emoji
+yes | sudo pacman -S picom dunst libnotify yt-dlp ffmpeg python-pip gnome-keyring man-db
 sleep 3
 clear
 
